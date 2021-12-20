d2f07714fd33fba726f4b2711cfcd0fcd613c001
YelyzavetaLukianova <yelyzaveta.lukianova@gmail.com>
2021-12-20 16:33:44 +0300

    hw4parttwo

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,16 +1,59 @@
 import ContactForm from '../ContactForm/ContactForm';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import * as storage from '../../services/LocalStorage';
 import Filter from '../Filter/Filter';
 import ContactList from '../ContactList/ContactList';
 
+// const Phonebook = () => {
+//   const [contacts, setContacts] = useState([
+//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//   ]);
+//   const [filter, setFilter] = useState('');
+
+// componentDidMount() {
+//   const savedContacts = storage.get('contacts');
+//   if(savedContacts){this.setState({ contacts: savedContacts })};
+// }
+
+// useEffect(() => {
+//   const savedContacts = storage.get('contacts');
+//   if (savedContacts) {
+//     setContacts({ contacts: savedContacts });
+//   }
+//   return;
+// }, []);
+
 const Phonebook = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(
+    () =>
+      storage.get('contacts') ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ],
+  );
   const [filter, setFilter] = useState('');
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.contacts !== this.state.contacts) {
+  //     storage.save('contacts', this.state.contacts);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   if (prevState.contacts !== contacts) {
+  //     setContacts('contacts', contacts);
+  //   }
+  //   return;
+  // }, [contacts]);
+
+  useEffect(() => {
+    storage.save('contacts', contacts);
+  }, [contacts]);
 
   const addNewContact = newContact => {
     const isDuplicate = contacts.some(
